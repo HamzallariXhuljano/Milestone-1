43fac36cbf13707afa1039eadb6f4f5d41d8e180
D3X <xhuljanohamzallari42@gmail.com>
2025-01-14 19:11:25 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+ #ifndef BUFFER_SIZE
+ #define BUFFER_SIZE 10000000000
+ #endif
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
