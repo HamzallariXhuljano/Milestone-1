a7a45fb18b7e866d29f5200abf5a8d80b3012cac
D3X <xhuljanohamzallari42@gmail.com>
2025-01-14 19:10:30 +0100

    Update ft_get_next_line.c

--- get_next_line/ft_get_next_line.c
+++ get_next_line/ft_get_next_line.c
@@ -11,7 +11,56 @@
 /* ************************************************************************** */
 
 
+#include "get_next_line.h"
+
+char	*read_ln(int fd, char *str)
+{
+	char *buffer;
+	size_t	nbr_read;
+	char *temp;
+
+	buffer =ft_calloc((BUFFER_SIZE + 1 ), 1);
+	temp = NULL;
+	nbr_read = 2;
+	if(!buffer)
+		return (NULL);
+	while (1)
+	{
+		if (nbr_read <= 0)
+			return (free(buffer), free(temp),NULL);
+		nbr_read = read(fd, buffer, BUFFER_SIZE);
+		temp = ft_strjoin(str, buffer);
+		str = temp;
+		if (ft_strchr(str, '\n'))
+			break;
+	}
+	free(buffer);
+	buffer = NULL;
+	return ( str);
+}
+
 char	*get_next_line(int fd)
 {
+	static char *str;
+	char *line;
 
+	if (!str)
+		str = ft_strdup("");
+	line = read_ln(fd, str);
+	if(!line)
+		return(free(str), NULL);
+	return (line);
+}
+int main()
+{
+	int	fd = open("text.txt", O_RDWR);
+	char *line;
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
