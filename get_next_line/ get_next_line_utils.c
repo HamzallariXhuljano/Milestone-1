5a0a9bac6291a49d0b51a40442f06ba5c291b710
D3X <xhuljanohamzallari42@gmail.com>
2025-01-14 19:11:08 +0100

    Update  get_next_line_utils.c

--- get_next_line/ get_next_line_utils.c
+++ get_next_line/ get_next_line_utils.c
@@ -10,3 +10,103 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = malloc((len_s1 + len_s2) * sizeof(char) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len_s1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len_s2)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		i;
+
+	str = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*input;
+	size_t	result;
+	size_t	i;
+
+	i = 0;
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	result = nmemb * size;
+	if (result / nmemb != size)
+		return (NULL);
+	input = malloc(nmemb * size);
+	if (input == NULL)
+		return (NULL);
+
+	while (i < result)
+	{
+			input[i++] = '\0';
+	}
+	return (input);
+}
