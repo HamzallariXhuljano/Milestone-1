fcdd1009504e49d87eadce64664ebbe0795dc11b
Xhuljano Hamzallari <xhamzall@c2r2p6.42firenze.it>
2025-01-15 19:15:19 +0100

    quasi alla fine

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,37 +6,91 @@
 /*   By: xhamzall <xhamzall@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 23:13:13 by xhamzall          #+#    #+#             */
-/*   Updated: 2025/01/15 14:11:31 by xhamzall         ###   ########.fr       */
+/*   Updated: 2025/01/15 18:23:51 by xhamzall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
 
-char	*read_ln(int fd, char *str)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*str_src;
+	unsigned char		*str_dest;
+	size_t				i;
+
+	i = 0;
+	str_dest = dest;
+	str_src = src;
+	if (dest == NULL && src == NULL && n > 0)
+		return (NULL);
+	while (i < n)
+	{
+		str_dest[i] = str_src[i];
+		i++;
+	}
+	return (dest);
+}
+
+static char	*read_ln(int fd, char *str)
 {
 	char *buffer;
-	size_t	nbr_read;
+	ssize_t	nbr_read;
 	char *temp;
 
+	if (!str)
+		str = ft_strdup("");
 	buffer =ft_calloc((BUFFER_SIZE + 1 ), 1);
 	temp = NULL;
-	nbr_read = 2;
+	nbr_read = 0;
 	if(!buffer)
 		return (NULL);
 	while (1)
 	{
-		if (nbr_read <= 0)
-			return (free(buffer), free(temp),NULL);
 		nbr_read = read(fd, buffer, BUFFER_SIZE);
+		if (nbr_read ==-1)
+			return (free(buffer), free(str),NULL);
+		if (nbr_read == 0)
+			break;
 		temp = ft_strjoin(str, buffer);
 		str = temp;
 		if (ft_strchr(str, '\n'))
 			break;
 	}
-	free(buffer);
-	buffer = NULL;
-	return ( str);
+	return (free (temp), free(buffer),str);
+}
+static char	*make_ln(char *str)
+{
+	size_t	i;
+	size_t	new_ln;
+	char	*mk_line;
+
+	i = 0;
+	new_ln = 0;
+	if(!str)
+		return (NULL);
+	while (str[i] && str[i] != '\n' )
+		i++;
+	if (str[i] == '\n')
+		new_ln++;
+	mk_line = malloc((new_ln + i) * sizeof(char) + 1);
+	if (!mk_line)
+		return (NULL);
+	ft_memcpy(mk_line, str, i + new_ln);
+	mk_line[i + new_ln] = '\0';
+	return (mk_line);
+}
+static char	*prev (char *str)
+{
+	size_t	i;
+	char	*tmp;
+
+	i = 0;
+	while (str[i] && str[i] != '\n' )
+		i++;
+	if (str[i] == '\0' || str[i + 1] == '\0')
+		return(free(str), NULL);
+	tmp = ft_strdup(&str[i+1]);
+	return(free(str), tmp);
 }
 
 char	*get_next_line(int fd)
@@ -44,24 +98,27 @@ char	*get_next_line(int fd)
 	static char *str;
 	char *line;
 
-	if (!str)
-		str = ft_strdup("");
-	line = read_ln(fd, str);
-	if(!line)
-		return(free(str), NULL);
+	str = read_ln(fd, str);
+	if(!str)
+		return(NULL);
+	line = make_ln(str);
+	if(!line || line[0] == 0)
+		return(free(str), free (line), NULL);
+	str = prev(str);
 	return (line);
 }
-// int main()
-// {
-// 	//int	fd = open("text.txt", O_RDWR);
-// 	char *line;
+  int	main(void)
+{
+	int	fd;
+	char *line;
 
-// 	while ((line = get_next_line(0)) != NULL)
-// 	{
-// 		printf("%s", line);
-// 		free(line);
-// 	}
-// 	free(line);
-// 	//close(fd);
-// 	return (0);
-// }
+	fd = open("txt.txt", O_RDWR);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
+
